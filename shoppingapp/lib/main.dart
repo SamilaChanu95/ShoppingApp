f8c5e72f42d150af865cdd10d82c59b21e27a085
SamilaChanu95 <chanukasamila@gmail.com>
2019-08-25 20:46:48 +0530

    Create stateful widget in main.dart file

--- shoppingapp/lib/main.dart
+++ shoppingapp/lib/main.dart
@@ -11,10 +11,50 @@ class ShoppingApp extends StatelessWidget {
     // TODO: implement build
 
     // when we generate material app then should use
-      return MaterialApp{
-        //avoid the debug tag
-        debug
+      return MaterialApp(
 
-      }
+        //avoid the debug tag
+        debugShowCheckedModeBanner: false,
+
+        home: MyHomePage(),
+
+      );
     }
   }
+
+  //generate stateful   widget for MyHomePage
+  class MyHomePage extends StatefulWidget {
+
+    @override
+    _MyHomePageState createState() => _MyHomePageState();
+  }
+
+  class _MyHomePageState extends State<MyHomePage> {
+
+    //generate widget because that is stateful widget
+    @override
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      return Scaffold(
+        body: ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Stack(
+                  children: <Widget>[
+                    Container(
+                      height: 200.0,
+                      width: double.infinity,
+                      color: Colors.pinkAccent ,
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        )
+      );
+   }
+  }
+
+
